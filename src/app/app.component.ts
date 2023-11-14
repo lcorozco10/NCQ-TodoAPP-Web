7ea345071afc17fe2aff2fd018aca2f9cd042d74
lcorozco <luis.orozco@nativo.la>
2023-11-14 12:53:03 -0600

    define page style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ import { initFlowbite } from 'flowbite';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'TodoApp';
+  title = 'NCQ Todo App';
   ngOnInit(): void {
     initFlowbite();
   }
